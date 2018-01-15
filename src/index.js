fabe5c02da88e46ed88f276877644ff60d53690f
Jeff Paltera <iam@jeffersonpaltera.com>
2018-01-15 13:14:15 +0800

    update styling and babel config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import axios from 'axios'
+import vex from 'vex-js'
+import "regenerator-runtime/runtime";
+import 'vex-js/dist/css/vex-theme-default.css'
+import 'vex-js/dist/css/vex.css'
+
+vex.registerPlugin(require('vex-dialog'));
+vex.defaultOptions.className = 'vex-theme-default'
 
 const LoadJS = (url) => {
   return new Promise((resolve, reject) => {
@@ -39,7 +46,11 @@ class Coupon {
     referredUrl, // [Optional] Referred Url or extra data want to include/track
     redirectUrl, // [Optional] Coupon success redirect url
     multiple, // [Optional] If multiple store
-    preview // [Optional] , Default: true 
+    preview, // [Optional] , Default: true
+    dsp,
+    exchange,
+    inventory,
+    campaignId
   }) {
     try {
       this.config = {
@@ -48,11 +59,17 @@ class Coupon {
         tabId,
         trackId,
         form,
-        uniqueCode
+        uniqueCode,
+        campaignId,
+        dsp,
+        exchange,
+        inventory
       }
 
+      this.code = null;
+
       this.preview = preview || false;
-      
+
       // Check every config that it is existing / these are the required fields
       const configCheck = Object.keys(this.config).filter(key => this.config[key] === undefined || !this.config[key])
       if (configCheck.length > 0) {
@@ -62,9 +79,9 @@ class Coupon {
         }, '')
         throw new Error('The following are needed in Coupon instance: ' + neededConfig)
       }
-  
+
       if (form.id) {
-        this.form = window.document.getElementById(form.id);  
+        this.form = window.document.getElementById(form.id);
       } else {
         throw new Error('We need id property for form object. Please enter the id for your form.')
       }
@@ -74,7 +91,7 @@ class Coupon {
       if (this.submitBtn.length !== 1) {
         throw new Error('We need at least/only one type=submit/type=image in your form.')
       }
-  
+
       let fieldCheck = [];
       this.fields = Object.values(this.form.elements).filter(elem => {
         if (!elem.name && elem.nodeName === 'INPUT' && elem.type !== 'image') throw new Error('We need name attribute for every input field in your form. Check every input.')
@@ -90,7 +107,7 @@ class Coupon {
         const missing = diff(['name', 'phoneNo', 'email'], fieldCheck)
         throw new Error('We need the required fields named by (name, phoneNo, email) and you\'re missing ' + missing.join(','))
       }
-  
+
       this.recipients = recipients || ''
       this.referredUrl = referredUrl || ''
       this.trackers = trackers || false;
@@ -101,6 +118,7 @@ class Coupon {
 
       if (this.trackers.impression && !this.preview) {
         imgTrack(this.trackers.impression);
+        this.track('impression');
       }
 
       if (this.landing && this.landing.id && this.landing.url) {
@@ -109,11 +127,12 @@ class Coupon {
           window.location = this.landing.url
           if (this.trackers.landing && !this.preview) {
             imgTrack(this.trackers.landing)
+            this.track('landing');
           }
         }
       }
-  
-      this.form.onsubmit = this.submit.bind(this)
+
+      this.form.onsubmit = this.submitCoupon.bind(this)
       window.leadGenCallback = this.handleLeadgenCallback.bind(this)
     } catch (e) {
       console.error(e);
@@ -136,8 +155,58 @@ class Coupon {
     })
   }
 
-  async submit (e) {
+  track (type) {
+    const t = document.createElement('img');
+    const cb = new Date().valueOf();
+    const src = `https://www.cdn.serving1.net/a/analytic.htm?uid=0&isNew=false&rmaId=${this.config.studioId}&domainId=0&pageLoadId=${cb}&userId=${this.config.userId}&pubUserId=0&campaignId=${this.config.campaignId}&browser=&os=&domain=&callback=trackSuccess&callback=trackSuccess&type=${type}&tt=E&cb=${cb}&p=1&dsp=${this.config.dsp}&exchange=${this.config.exchange}&inventory=${this.config.inventory}&uniqueId=${this.form.elements[this.config.uniqueCode].value}`;
+    t.src = src;
+    t.style.display = 'none';
+    document.body.appendChild(t);
+  }
+
+  async submitCoupon(e) {
     this.submitBtn[0].setAttribute('disabled', 'disabled');
+    this.submitBtn[0].style.opacity = 0.5;
+
+    e.preventDefault();
+    try {
+      let data = {
+        status: true,
+        code: this.form.elements[this.config.uniqueCode].value
+      }
+
+      if (!this.preview) {
+        const result = await axios.post(`https://www.mobileads.com/api/coupon/generate_coupon?uniqueCode=${this.form.elements[this.config.uniqueCode].value}&userId=${this.config.userId}&studioId=${this.config.studioId}&email=${this.form.elements['email'].value}&name=${this.form.elements['name'].value}&phoneNo=${this.form.elements['phoneNo'].value}`)
+        data = result.data;
+      }
+
+      if (data.status) {
+        if (data.code) {
+          this.code = data.code;
+          if (this.trackers.submit && !this.preview) {
+            imgTrack(this.trackers.submit)
+            this.track('submit')
+          }
+          this.submit();
+        } else {
+          vex.dialog.alert(data.message);
+          this.submitBtn[0].style.opacity = 1;
+          this.submitBtn[0].removeAttribute('disabled');
+          // if (this.config.form.callback) this.config.for`m.callback(data.message);
+        }
+      } else {
+        if (this.config.form.callback) this.config.form.callback(data.message);
+        throw new Error(data.message)
+      }
+      return await false
+    } catch (err) {
+      if (this.config.form.callback) this.config.form.callback(err);
+      console.error(err)
+      return await false
+    }
+  }
+
+  async submit (e) {
     try {
       let submissionUrl = `https://www.mobileads.com/api/save_lf?contactEmail=${this.recipients || ''}&gotDatas=1`
 
@@ -152,7 +221,7 @@ class Coupon {
         `.replace(/\n|\s/g, '')
       }, '') + ']'
 
-      submissionUrl += `&elements=${elements}`
+      submissionUrl += `&element=${elements}`
       submissionUrl += `&user-id=${this.config.userId}`
       submissionUrl += `&studio-id=${this.config.studioId}`
       submissionUrl += `&tab-id=${this.config.tabId}`
@@ -160,7 +229,6 @@ class Coupon {
       if (this.referredUrl) submissionUrl += `&referredURL=${this.referredUrl}`
       submissionUrl += `&callback=leadGenCallback`
 
-      e.preventDefault();
       if (this.preview) {
         this.handleLeadgenCallback({
           status: true
@@ -170,7 +238,6 @@ class Coupon {
       }
       return await false;
     } catch(err) {
-      e.preventDefault();
       console.error(err)
       return await false;
     }
@@ -180,46 +247,22 @@ class Coupon {
     try {
       if (!res.status) throw new Error('Failed to submit leadgen data.')
 
-      let data = {
-        status: true,
-        code: this.form.elements[this.config.uniqueCode].value
-      }
-
-      if (!this.preview) {
-        const result = await axios.post(`https://www.mobileads.com/api/coupon/generate_coupon?phoneNo=${this.form.elements['phoneNo'].value}&userId=${this.config.userId}&studioId=${this.config.studioId}&email=${this.form.elements['email'].value}&name=${this.form.elements['name'].value}`)
-        data = result.data;
-      }
-
-      if (data.status) {
-        if (data.code) {
-          if (this.trackers.submit && !this.preview) {
-            imgTrack(this.trackers.submit)
-          }
-
-          if (this.redirectUrl) {
-            if (this.trackers.redirect && !this.preview) {
-              imgTrack(this.trackers.redirect)
-            }
-            window.location = `${this.redirectUrl}?${this.config.uniqueCode}=${data.code}`
-          }
-
-          if (this.config.form.callback) {
-            const callbackResult = this.config.form.callback(null, true);
-            if (callbackResult && this.config.form.redirectUrl) {
-              setTimeout(() => {
-                window.location = `${this.config.form.redirectUrl}?${this.config.uniqueCode}=${data.code}`
-              }, 1000);
-            }
-          }
-        } else {
-          window.alert(data.message)
-          if (this.config.form.callback) this.config.form.callback(data.message);
+      if (this.redirectUrl) {
+        if (this.trackers.redirect && !this.preview) {
+          imgTrack(this.trackers.redirect)
+          this.track('redirect')
         }
-      } else {
-        if (this.config.form.callback) this.config.form.callback(data.message);
-        throw new Error(data.message)
+        window.location = `${this.redirectUrl}?uniqueCode=${this.code}`
       }
 
+      if (this.config.form.callback) {
+        const callbackResult = this.config.form.callback(null, true);
+        if (callbackResult && this.config.form.redirectUrl) {
+          setTimeout(() => {
+            window.location = `${this.config.form.redirectUrl}?uniqueCode=${this.code}`
+          }, 1000);
+        }
+      }
     } catch(err) {
       if (this.config.form.callback) this.config.form.callback(err);
       console.error(err)
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,31 @@
 const path = require('path')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: ['babel-polyfill', './src/index.js'],
+  entry: ['./src/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'coupon.min.js'
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            plugins: ["transform-regenerator"]
+          }
+        }
+      }, {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
     ]
   },
+  plugins: [new UglifyJSPlugin()],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     port: 9000
